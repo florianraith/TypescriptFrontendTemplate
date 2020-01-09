578e2e0ac2ea56b0bba8ecb5a8265b0edc3af545
Florian Raith <florianraith00@gmail.com>
2020-01-09 19:00:41 +0100

    fix dev server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const compiler = webpack(config);
 
 const app = express();
 app.use(morgan('tiny'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'static')));
 app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
 
 const server = http.createServer(app);
